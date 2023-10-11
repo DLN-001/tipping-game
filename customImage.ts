66a604e22839fe63a61a1d177d66c03851425e28
DLN-001 <77555673+DLN-001@users.noreply.github.com>
2023-10-10 23:13:06 -0500

    Update pxt.json, main.blocks, main.ts, customImage.ts

--- customImage.ts
+++ customImage.ts
@@ -2,8 +2,8 @@ namespace screen {
     /**
     * Computes the famous Fibonacci number sequence!
     */
-    //% block
-    export function screenArray(): string[][] {
+    //% block="LED|state" blockGap=8
+    export function ledState(): string[][] {
         return [
             initializeColumnArray2(),
             initializeColumnArray2(),
@@ -12,10 +12,55 @@ namespace screen {
             initializeColumnArray2()
         ]
     }
+
+    /**
+    * Computes the famous Fibonacci number sequence!
+    */
+    //% block="Create|Screen|Image" blockGap=8
+    export function createScreenImage() : Image {
+        let test = led.screenshot()
+        return led.screenshot()
+    }
+
+    /**
+    * Computes the famous Fibonacci number sequence!
+    */
+    //% block="Compare|LED|State %ledState1 %ledState2" blockGap=8
+    export function compareLEDState(ledState1: string[][], ledState2: string[][]) : boolean {
+        let ledState1String = ""
+        let ledState2String = ""
+        ledState1.forEach((element) => element.forEach((element) => ledState1String = ledState1String + element))
+        ledState2.forEach((element) => element.forEach((element) => ledState2String = ledState2String + element))
+        return ledState1String === ledState2String
+    }
+
+    /**
+    * Computes the famous Fibonacci number sequence!
+    */
+    //% block="Current|LED|State" blockGap=8
+    export function currentLEDState() : string[][] {
+        let myReturn = ledState()
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                if (led.pointBrightness(i, j) > 0) {
+                    myReturn[i][j] = "#"
+                }
+            }
+        }
+
+        return myReturn
+    }
 }
 
 // Add your code here
 function initializeColumnArray2() {
+    let image1 = images.createImage(`
+    #...#
+    .#.#.
+    ..#..
+    .#.#.
+    #...#
+    `)
     return [
         ".",
         ".",
--- main.ts
+++ main.ts
@@ -1,39 +1,35 @@
 input.onButtonPressed(Button.A, function () {
-	
+    if (screen.compareLEDState(savedLEDState, screen.currentLEDState())) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
 input.onGesture(Gesture.LogoUp, function () {
-	
+    basic.showIcon(IconNames.SmallHeart)
 })
 input.onGesture(Gesture.TiltLeft, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showIcon(IconNames.Heart)
 })
 input.onGesture(Gesture.TiltRight, function () {
 	
 })
 input.onGesture(Gesture.LogoDown, function () {
-	
+    basic.showIcon(IconNames.House)
 })
-function initializeColumnArray () {
-    return [
-    ".",
-    ".",
-    ".",
-    ".",
-    "."
-    ]
-}
-let myScreenArray = screen.screenArray()
-let image1 = images.createImage(`
-    # . . . #
-    . # . # .
-    . . # . .
-    . # . # .
-    # . . . #
-    `)
-image1.showImage(0)
+let savedLEDState: string[][] = []
+basic.showIcon(IconNames.Heart)
+savedLEDState = screen.currentLEDState()
+basic.pause(1000)
+basic.clearScreen()
+basic.pause(1000)
+led.plot(2, 1)
+led.plot(1, 2)
+led.plot(3, 2)
+led.plot(2, 3)
 basic.forever(function () {
 	
 })
